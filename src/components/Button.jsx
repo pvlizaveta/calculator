d987affcfd70739f321b9416bd709f267ee4863c
pvlizaveta <pv.lizaveta@gmail.com>
2021-06-27 20:18:15 -0400

    improved outlook

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,17 +1,17 @@
 import React from "react";
 import "./Button.css";
 
-const isOperator =val => {
-    return !isNaN(val) || val ==="." || val ==="="  
+const isOperator = val => {
+  return !isNaN(val) || val === "."
 }
+
 const Button = props => (
- <div 
-  className={`button-wrapper ${
-    isOperator(props.children)? null : "operator"
-  }`}
-   onClick={() => props.handleClick(props.children)}
- >
+  <div
+    className={`button-wrapper ${isOperator(props.children) ? null : "operator"
+      }`}
+    onClick={() => props.handleClick(props.children)}
+  >
     {props.children}
- </div>  
+  </div>
 )
 export default Button;
