ab3812a226bc73b2261ae0ea2d3f3f6fd247b1b4
pvlizaveta <59615682+pvlizaveta@users.noreply.github.com>
2021-07-19 20:54:11 -0400

    applied changes to layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,68 +1,67 @@
-import React, { Component } from "react"
-import './App.css';
-import Button from './components/Button'
-import { Input } from './components/Input'
-import { ClearButton } from "./components/ClearButton"
-import * as math from "mathjs"
-
+import React, { Component } from "react";
+import "./App.css";
+import Button from "./components/Button";
+import { Input } from "./components/Input";
+import { ClearButton } from "./components/ClearButton";
+import * as math from "mathjs";
 
 class App extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      input: ""
+      input: "",
     };
   }
 
-  addToInput = val => {
+  addToInput = (val) => {
     if (isNaN(val) && isNaN(this.state.input[this.state.input.length - 1])) {
       this.setState({ input: this.state.input });
-    }
-    else {
+    } else {
       this.setState({ input: this.state.input + val });
     }
-  }
+  };
 
   handleEqual = () => {
     if (isNaN(this.state.input[this.state.input.length - 1])) {
       this.setState({ input: this.state.input });
-    }
-    else {
+    } else {
       this.setState({ input: math.evaluate(this.state.input) });
     }
-  }
+  };
 
   render() {
     return (
       <div className="app">
-        <div className="calc-wrapper">
+        <div className="container">
           <Input input={this.state.input}></Input>
           <div className="row">
-            <div className="column">
+            <div className="col-3">
               <Button handleClick={this.addToInput}>+</Button>
               <Button handleClick={this.addToInput}>7</Button>
               <Button handleClick={this.addToInput}>4</Button>
               <Button handleClick={this.addToInput}>1</Button>
               <Button handleClick={this.addToInput}>0</Button>
             </div>
-            <div className="column">
+            <div className="col-3">
               <Button handleClick={this.addToInput}>-</Button>
               <Button handleClick={this.addToInput}>8</Button>
               <Button handleClick={this.addToInput}>5</Button>
               <Button handleClick={this.addToInput}>2</Button>
               <Button handleClick={this.addToInput}>.</Button>
             </div>
-            <div className="column">
+            <div className="col-3">
               <Button handleClick={this.addToInput}>*</Button>
               <Button handleClick={this.addToInput}>9</Button>
               <Button handleClick={this.addToInput}>6</Button>
               <Button handleClick={this.addToInput}>3</Button>
-              <ClearButton handleClear={() => this.setState({ input: "" })}>Clear</ClearButton>
+              <ClearButton handleClear={() => this.setState({ input: "" })}>
+                Clear
+              </ClearButton>
             </div>
-            <div className="column">
+            <div className="col-3">
               <Button handleClick={this.addToInput}>/</Button>
-              <div className="love">
+              <div className="equal">
                 <Button handleClick={() => this.handleEqual()}>=</Button>
               </div>
             </div>
